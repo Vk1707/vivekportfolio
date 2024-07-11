58c22b25b110f638301f06946156c899467d6625
vivek kumar <vivekkumarvk1707@gmail.com>
2024-07-11 19:46:51 +0530

    Change

--- GoogleSheet.js
+++ GoogleSheet.js
@@ -1,11 +1,14 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbzhM3-MHm7MHz0K-BmnUZab98Znzklhhes2FuheAPM9Hx9SHYhsG8cghGEM7E2pup96lg/exec'
 
 const form = document.forms['contact-form']
+const subButton = document.getElementById("subButton");
 
 form.addEventListener('submit', e => {
  e.preventDefault()
+ subButton.innerHTML = "Submiting.."
  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
- .then(response => alert("Thank you! your form is submitted successfully." ))
+ .then(response => alert("Thank You! Your Form is Submitted Successfully." ))
  .then(() => { window.location.reload(); })
  .catch(error => console.error('Error!', error.message))
 })
+
